022665b0701cb95a47325e4ee41f4dcd8af4489f
denzalovera <denz.alovera@gmail.com>
2016-12-17 14:25:09 +0800

    initial setup

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return "hello world!"
 
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
     app.run()
